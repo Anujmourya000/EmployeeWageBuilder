bb7828a9680dcfaa2d0555290998486533858983
Anujmourya000 <Anujmourya000@gmail.com>
2021-04-26 15:08:02 +0530

    Added: Welcome message is added

---
+++ EmpWageBuilder.java
@@ -0,0 +1,9 @@
+
+public class EmpWageBuilder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Employee Wage Builder");
+	}
+
+}
