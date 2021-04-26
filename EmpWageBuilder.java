f497b332fdaa2af822876c9432097a312182ff00
Anujmourya000 <Anujmourya000@gmail.com>
2021-04-26 16:41:34 +0530

    Added: Wages for a month are added

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -4,7 +4,21 @@ public class EmpWageBuilder {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Solving the problem with Switch Cases");
+		int Full_time_employee = 1;
+		int Part_time_employee = 2;
+		int Full_time_Wage = 20;
+		int Full_time_Hour = 8;
+		int Part_time_Hour = 4;
+		
+		// Total working days for a month
+		int Working_days = 20;
+		
+		double Emp_Wage1 = (Working_days * Full_time_Wage * Full_time_Hour);
+		
+		double Emp_Wage2 = (Working_days * Full_time_Wage * Part_time_Hour);
+		
+		System.out.println("Full Time Employee Wage for a month is = " +Emp_Wage1 );
+		System.out.println("Part Time Employee Wage for a month is = " +Emp_Wage2);
 	}
 
 }
